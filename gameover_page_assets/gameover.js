83468de5aa3c8d06ba7bce9744e16400b7aae705
Nicolette <peter@Peters-MBP.lan>
2023-04-06 00:38:48 -0400

    added additional game sections in html, added custom css and materialize styling, began JS functionality in Finale page

--- gameover_page_assets/gameover.js
+++ gameover_page_assets/gameover.js
@@ -1,10 +1,12 @@
-var header = $('.game-over')
+var dynamicSection = $('.dynamic-message')
 var userMessage = $('#user-message');
-var input = window.prompt("Enter game status");
+// Need to remove/replace
+var input = window.prompt("Type 'win' or 'lose'");
 
+// Need to replace with game data
 if (input === 'win'){
     var winText = userMessage.text("You're on a roll Bro!");
 } else if (input === 'lose'){
     var loseText = userMessage.text("Oh snap! Better luck next time Bro");
 }
-header.append(userMessage);
+dynamicSection.append(userMessage);
